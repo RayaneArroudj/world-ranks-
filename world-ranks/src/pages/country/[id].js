9d29f0638caf97f7cdddbd89bcf0ab0bfefeb75b
RayaneArroudj <rayane.arroudj@gmail.com>
2022-07-28 08:58:45 +0200

    mapping sur les resultats de la page id

--- world-ranks/src/pages/country/[id].js
+++ world-ranks/src/pages/country/[id].js
@@ -1,14 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from '../../components/Layout/Layout';
+import styles from './country.module.css';
 
 const Country = ({ country }) => {
   console.log(country);
-  return <Layout title={country.name}>{country.name}</Layout>;
+  return (
+    <div>
+      {country.map((country, id) => {
+        return (
+          <Layout key={id} title={country.name.common}>
+            <div className={styles.overview_panel}>
+              <img src={country.flags.png} alt={country.name.common} />
+              <h1 className={styles.overview_name}>{country.name.common}</h1>
+              <div className={styles.overview_region}>{country.region}</div>
+
+              <div className={styles.overview_numbers}>
+                <div className={styles.overview_population}>
+                  <div className={styles.overview_value}>
+                    {country.population}
+                  </div>
+                  <div className={styles.overview_label}>Population</div>
+                </div>
+                <div className={styles.overview_area}>
+                  <div className={styles.overview_value}>{country.area}</div>
+                  <div className={styles.overview_label}>Area</div>
+                </div>
+              </div>
+            </div>
+          </Layout>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Country;
 
 export const getServerSideProps = async ({ query: { id } }) => {
-  const res = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
+  const res = await fetch(`http://restcountries.com/v3.1/alpha/${id}`);
 
   const country = await res.json();
 
